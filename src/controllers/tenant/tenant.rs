a1e9f3d1056660ddd1f555dc3c28610bc206dfee
siddh34 <ssiddharthsutar@gmail.com>
2024-10-10 11:43:08 +0530

    chore: better error handling

--- src/controllers/tenant/tenant.rs
+++ src/controllers/tenant/tenant.rs
@@ -6,6 +6,7 @@ use diesel_async::RunQueryDsl;
 use crate::utils::generate_random_hash::generate_random_hash_function;
 use crate::utils::generate_short_hash::encrypt;
 use std::env;
+use rocket::error;
 
 #[derive(serde::Deserialize)]
 pub struct NewTenantCreate<'a> {
@@ -21,29 +22,35 @@ pub struct CreateTenantResponse {
 #[post("/createTenant", data = "<new_tenant_create>")]
 pub async fn create_tenant(new_tenant_create: Json<NewTenantCreate<'_>>) -> Json<CreateTenantResponse> {
 
-    let size: String = env::var("SIZE_LEN_LIMIT_STR").expect("Size must be set");
+    let size: Result<String, String> = env::var("SIZE_LEN_LIMIT_STR").map_err(|e|{
+        error!("Error: {}", e);
+        "Size must be set".to_string()
+    });
 
-    let rand_hash: String = generate_random_hash_function(size.parse().unwrap());
+    let rand_hash: String = generate_random_hash_function(size.unwrap().parse().unwrap());
 
     let new_tenant: NewTenant<'_> = NewTenant {
         id: rand_hash.as_str(),
         name: new_tenant_create.name,
     };
 
-    diesel::insert_into(tenant)
+    let _ = diesel::insert_into(tenant)
         .values(new_tenant)
         .execute(&mut establish_connection().await)
         .await
-        .expect("Error saving new tenant");
+        .map_err(|e| {
+            error!("Error saving new tenant: {}", e);
+            "Error saving new tenant".to_string()
+        });
 
-    let key = env::var("ENCRYPTION_KEY").expect("Key must be set");
+    let key: String = env::var("ENCRYPTION_KEY").expect("Key must be set");
 
     println!("Key: {}", key);
 
-    let encrypted_text = encrypt(rand_hash.as_str(), key.as_str(), 16);
+    let encrypted_text: String = encrypt(rand_hash.as_str(), key.as_str(), 16);
 
     Json(CreateTenantResponse {
         action: "Created Tenant successfully!".to_string(),
-        encrypted_text: encrypted_text,
+        encrypted_text,
     })
 }
